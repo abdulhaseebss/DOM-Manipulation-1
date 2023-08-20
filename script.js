806ee925617fa8433fe64d67e8286848b452d709
abdulhaseebss <m.saleemhaseeb@gmail.com>
2023-08-20 15:10:55 +0500

    Dom Problems

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// Q=1
+
+
+// // Select the paragraph element by its ID
+// const paragraphElement = document.getElementById("myParagraph");
+
+// // Change the text content of the paragraph
+// paragraphElement.textContent = "Hello, DOM!";
+
+
+// Q=2
+
+// function changeColor() {
+//     const myDiv = document.getElementById("myDiv");
+//     myDiv.style.backgroundColor = "blue";
+// }
+
+
+// Q=3
+
+// function showText() {
+//     var hiddenText = document.getElementById("hiddenText");
+//     hiddenText.style.display = "block";
+// }
